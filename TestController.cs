fc59f03fad13b5c08022669da11bafa225584d3b
sulakkhanak <51056097+sulakkhanak@users.noreply.github.com>
2019-05-27 02:04:29 +0530

    Add files via upload

---
+++ TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvc_Test.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPerson()
+        {
+            return View();
+        }
+
+        public ActionResult ShowDepartment()
+        {
+            return View();
+        }
+
+	}
+}
